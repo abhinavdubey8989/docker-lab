e04f156be5ebc5ad8ced87e6e74974281b124728
abhinavdubey8989 <abhinavdubey8989@gmail.com>
2026-06-13 22:18:01 +0530

    Add script to setup control node

---
+++ scripts/setup_control_node.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+# ============================================================================
+# [Aim]
+# This script will setup the node from where the ansible & terrafom is run
+#
+# [Usage]
+#  - chmod +x <script>.sh
+#  - ./<script>.sh
+#
+# ============================================================================
+
+
+
+set -euo pipefail
+
+main() {
+
+  BASE_DIR="$(dirname "$0")"
+  echo "Base dir = $BASE_DIR"
+
+  echo "Installing Ansible..."
+  bash "$BASE_DIR/install_ansible.sh"
+
+  echo "Installing Terraform..."
+  bash "$BASE_DIR/install_terraform.sh"
+
+  echo "Installing AWS CLI..."
+  bash "$BASE_DIR/install_aws_cli.sh"
+
+  echo "🎉 Control-node setup completed"
+}
+
+main "$@"
